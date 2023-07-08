a2fce562fdbda6a6df722e0618422cd4feb46a3c
Calex Raffield <clxrffdman@gmail.com>
2023-07-07 20:21:20 -0700

    -random banner shit

--- Assets/Scripts/UI/BannerQuipController.cs
+++ Assets/Scripts/UI/BannerQuipController.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Febucci.UI;
 using TMPro;
 
 public class BannerQuipController : MonoBehaviour
 {
     public CanvasGroup canvasGroup;
     public TextMeshProUGUI bannerText;
+    public TypewriterByCharacter typewriter;
     public bool isQuipping = false;
+
+    private void Start()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+    }
 
     public void RequestBannerQuip(string quipText, float toDuration, float lingerDuration, float exitDuration)
     {
@@ -23,13 +31,19 @@ public class BannerQuipController : MonoBehaviour
     IEnumerator QuipRoutine(string quipText, float toDuration, float lingerDuration, float exitDuration)
     {
         isQuipping = true;
-        bannerText.text = quipText;
+        //typewriter.StopShowingText();
+        bannerText.text = "";
         canvasGroup.alpha = 0;
+        transform.localScale = new Vector3(1, 0, 1);
+        LeanTween.scaleY(gameObject, 1, toDuration);
         LeanTween.alphaCanvas(canvasGroup, 1, toDuration);
 
-        yield return new WaitForSeconds(toDuration + lingerDuration);
+        yield return new WaitForSeconds(toDuration);
+        typewriter.ShowText(quipText);
+        yield return new WaitForSeconds(lingerDuration);
 
         LeanTween.alphaCanvas(canvasGroup, 0, exitDuration);
+        LeanTween.scaleY(gameObject, 0, exitDuration);
 
         yield return new WaitForSeconds(toDuration + lingerDuration);
 
--- Assets/Scripts/UI/GameplayUIManager.cs
+++ Assets/Scripts/UI/GameplayUIManager.cs
@@ -8,4 +8,12 @@ public class GameplayUIManager : UnitySingleton<GameplayUIManager>
 {
     public PortraitController portraitController;
     public BannerQuipController bannerQuipController;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            bannerQuipController.RequestBannerQuip("Spared!", 0.2f, 3f, 0.1f);
+        }
+    }
 }
