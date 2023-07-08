4540fb2235865147a4e5cae778d4cff4d638c661
Calex Raffield <clxrffdman@gmail.com>
2023-07-07 21:11:19 -0700

    -fixed null ref

--- Assets/Scripts/LevelScripts/LevelManager.cs
+++ Assets/Scripts/LevelScripts/LevelManager.cs
@@ -11,7 +11,7 @@ public class LevelManager : UnitySingleton<LevelManager>
     // Start is called before the first frame update
     void Start()
     {
-        course = CourseController.Instance;
+
     }
 
     public void StartLevel() {
