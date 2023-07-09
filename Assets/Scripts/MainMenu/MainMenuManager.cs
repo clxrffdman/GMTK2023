b61cfab8a36857c676e883c92d026478e730ff4f
Calex Raffield <clxrffdman@gmail.com>
2023-07-09 03:01:04 -0700

    main menu buttons tweening

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -10,10 +10,17 @@ public class MainMenuManager : MonoBehaviour
     public List<Image> medalDisplays = new List<Image>();
     public List<Sprite> medalSprites;
 
+    public CanvasGroup promptGroup;
+    public CanvasGroup buttonGroup;
+    public bool hasPrompted = false;
     public GameObject hiddenCircuitButton;
 
     private void Start()
     {
+        promptGroup.alpha = 1;
+
+        LeanTween.value(this.gameObject, SetPromptAlpha, 0, 1, 0.8f).setLoopPingPong();
+
         bool showHidden = true;
 
         for(int i = 0; i < medalDisplays.Count; i++)
@@ -49,7 +56,7 @@ public class MainMenuManager : MonoBehaviour
                     return;
                 }
             }
-            else if (i != 3)
+            else if (i < 3)
             {
                 showHidden = false;
             }
@@ -57,6 +64,35 @@ public class MainMenuManager : MonoBehaviour
         }
 
         hiddenCircuitButton.SetActive(showHidden);
+    }
+
+    public void SetPromptAlpha(float alpha)
+    {
+        promptGroup.alpha = alpha;
+    }
+
+    public void MoveButtonX(float xVal)
+    {
+        buttonGroup.transform.GetComponent<RectTransform>().anchoredPosition = new Vector3(xVal, -654, 0);
+    }
+
+    public void Update()
+    {
+        if (Input.anyKeyDown && !hasPrompted)
+        {
+            hasPrompted = true;
+            LeanTween.cancel(this.gameObject);
+            StartCoroutine(TweenRoutine());
+        }
+    }
+
+
+    public IEnumerator TweenRoutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+        LeanTween.alphaCanvas(promptGroup, 0, 0.2f);
+        LeanTween.value(this.gameObject, MoveButtonX, -450, 190, 0.8f);
+
     }
 
     public void LoadCircuit(int index)
