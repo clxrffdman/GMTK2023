8e7162e5fb3132a690816b042cfe86d1a3e022fb
Calex Raffield <clxrffdman@gmail.com>
2023-07-08 19:33:43 -0700

    slow mod

--- Assets/Scripts/Balls/Modifiers/ModifierSlow.cs
+++ Assets/Scripts/Balls/Modifiers/ModifierSlow.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ModifierSlow : BallModifier
 {
+    public float slowAmount = 1;
+
     public override void OnUpdate(BallController controller, float activeTime, float coursePercentage)
     {
         base.OnUpdate(controller, activeTime, coursePercentage);
 
-        controller.rb.velocity = controller.rb.velocity * (1 - coursePercentage);
+        controller.rb.velocity = controller.rb.velocity - (controller.rb.velocity * coursePercentage * Time.deltaTime * slowAmount);
     }
 }
