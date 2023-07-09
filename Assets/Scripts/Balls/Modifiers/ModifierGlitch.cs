125bb04188f0961b4e29e508223117bdf5b64162
Calex Raffield <clxrffdman@gmail.com>
2023-07-08 22:27:30 -0700

    -glitch ball have fun

--- Assets/Scripts/Balls/Modifiers/ModifierGlitch.cs
+++ Assets/Scripts/Balls/Modifiers/ModifierGlitch.cs
@@ -10,13 +10,17 @@ public class ModifierGlitch : BallModifier
 
     private float counter = 0;
     public LayerMask teleportCheckLayerMask;
+    private bool isGlitching = false;
 
-    public GameObject teleportIndicator;
+    public GameObject teleportIndicatorPrefab;
+    public GameObject teleportIndicator = null;
 
     public override void OnSpawn(BallController controller)
     {
         base.OnSpawn(controller);
         counter = teleportInterval;
+        teleportIndicator = (teleportIndicator == null) ? GameObject.Instantiate(teleportIndicatorPrefab, controller.transform) : teleportIndicator;
+        GlobalFunctions.FadeOut(teleportIndicator.GetComponent<SpriteRenderer>(), 0.2f);
     }
 
     public override void OnUpdate(BallController controller, float activeTime, float coursePercentage)
@@ -27,54 +31,75 @@ public class ModifierGlitch : BallModifier
         {
             counter -= Time.deltaTime;
         }
-        else
+        else if(!isGlitching)
         {
-            Debug.Log("checking glitch validity");
+            isGlitching = true;
+            controller.StartCoroutine(GlitchTeleport(controller));
+        }
 
-            bool leftValid = false;
-            bool rightValid = false;
+    }
 
-            float newXPos = 0;
+    public IEnumerator GlitchTeleport(BallController controller)
+    {
+        Vector3 newPosOffset = Vector3.zero;
+        teleportIndicator.transform.localPosition = Vector3.zero;
+        Debug.Log("checking glitch validity");
 
-            RaycastHit2D leftHit = Physics2D.Raycast(controller.transform.position, -Vector3.right, teleportCheckLayerMask);
-            RaycastHit2D rightHit = Physics2D.Raycast(controller.transform.position, Vector3.right, teleportCheckLayerMask);
+        bool leftValid = false;
+        bool rightValid = false;
 
-            if(leftHit.distance > minTeleportDistance)
-            {
-                leftValid = true;
-            }
+        float newXPos = 0;
 
-            if (rightHit.distance > minTeleportDistance)
-            {
-                rightValid = true;
-            }
+        RaycastHit2D leftHit = Physics2D.Raycast(controller.transform.position, -Vector3.right, Mathf.Infinity, teleportCheckLayerMask);
+        RaycastHit2D rightHit = Physics2D.Raycast(controller.transform.position, Vector3.right, Mathf.Infinity, teleportCheckLayerMask);
 
-            if (leftValid && rightValid)
-            {
-                if(Random.Range(0, 2) == 0)
-                {
-                    newXPos = Random.Range(-minTeleportDistance, -leftHit.distance);
-                }
-                else
-                {
-                    newXPos = Random.Range(minTeleportDistance, rightHit.distance);
-                }
-            }
-            else if (leftValid)
+        if (leftHit.distance > minTeleportDistance)
+        {
+            leftValid = true;
+        }
+
+        if (rightHit.distance > minTeleportDistance)
+        {
+            rightValid = true;
+        }
+
+        if (leftValid && rightValid)
+        {
+            if (Random.Range(0, 2) == 0)
             {
                 newXPos = Random.Range(-minTeleportDistance, -leftHit.distance);
             }
-            else if (rightValid)
+            else
             {
                 newXPos = Random.Range(minTeleportDistance, rightHit.distance);
             }
-
-            controller.transform.localPosition += new Vector3(newXPos, 0, 0);
-
-
-
+        }
+        else if (leftValid)
+        {
+            newXPos = Random.Range(-minTeleportDistance, -leftHit.distance);
+        }
+        else if (rightValid)
+        {
+            newXPos = Random.Range(minTeleportDistance, rightHit.distance);
+        }
+        else
+        {
+            isGlitching = false;
             counter = teleportInterval;
+            yield break;
         }
 
+        newPosOffset = new Vector3(newXPos, 0, 0);
+
+        teleportIndicator.transform.localPosition += newPosOffset;
+
+        yield return GlobalFunctions.FadeIn(teleportIndicator.GetComponent<SpriteRenderer>(), 0.2f);
+
+        controller.transform.localPosition += newPosOffset;
+        teleportIndicator.GetComponent<SpriteRenderer>().color = teleportIndicator.GetComponent<SpriteRenderer>().color * new Color(1, 1, 1, 0);
+
+        counter = teleportInterval;
+
+        isGlitching = false;
     }
 }
